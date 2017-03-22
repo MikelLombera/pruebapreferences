a0f9f386f398e7e9c7f784c2aa99e97c1880a91f
Mikel-PC\Mikel <mi.lombera@gmail.com>
2017-03-22 12:16:55 +0100

    Version Prueba3

--- app/src/main/java/com/example/mikel/preferences/MainActivity.java
+++ app/src/main/java/com/example/mikel/preferences/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.mikel.preferences;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnPru;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPru = (Button)findViewById(R.id.btnPru);
     }
 }
